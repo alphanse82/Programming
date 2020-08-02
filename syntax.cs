9b1a7be9b03f53a1f4a4e73653f779e78f081d22
alphanse82 <RakeshKumarGupta@outlook.com>
2020-08-02 15:27:35 -0700

    Create syntax.cs

---
+++ syntax.cs
@@ -0,0 +1,7 @@
+ public IList<IList<int>> PathSum(TreeNode root, int sum) {
+    IList<IList<int>> result = new List<IList<int>>();
+         result.Add( new List<int>());
+     result[0].Add( int.Parse("5") );
+    return result;
+    
+ }
